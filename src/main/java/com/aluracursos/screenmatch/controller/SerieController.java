e8bab061dc13f334c576f38213dc25f88707be6e
SinnsykFG <felipegh.tmo@gmail.com>
2025-06-25 17:25:00 -0400

    Avance conexion frontend y backend.

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,13 +1,30 @@
 package com.aluracursos.screenmatch.controller;
 
+import com.aluracursos.screenmatch.dto.SerieDTO;
+import com.aluracursos.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+
 
 @RestController
 public class SerieController {
-
+    @Autowired
+    private SerieRepository serieRepository;
     @GetMapping("/series")
-    public String mostrarMensaje(){
-        return "Este es mi primer mensaje en mi aplicación web";
+    public List<SerieDTO> obtenerTodasLasSeries(){
+        return serieRepository.findAll().stream()
+                .map(serie -> new SerieDTO(
+                        serie.getTitulo(),
+                        serie.getTotalTemporadas(),
+                        serie.getEvaluacion(),
+                        serie.getPoster(),
+                        serie.getGenero(),
+                        serie.getActores(),
+                        serie.getSinopsis()))
+                .toList();
+
     }
 }
